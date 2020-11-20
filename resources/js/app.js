f13a73fc627d527aadcb729734bf2118a3e462a9
willywin99 <willywinsianturi@gmail.com>
2020-11-20 18:05:33 +0700

    Global VueJs Filters and MomentJs Instead of Carbon in Laravel and Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,7 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import moment from 'moment';
 import { Form, HasError, AlertError } from 'vform'
 
 window.Form = Form;
@@ -27,6 +28,14 @@ const router = new VueRouter({
   mode: 'history',
   routes
 })
+
+Vue.filter('upText', function(text){
+    return text.charAt(0).toUpperCase() + text.slice(1)
+});
+
+Vue.filter('myDate', function(created){
+    return moment(created).format('MMMM Do YYYY');
+});
 
 /**
  * The following block of code may be used to automatically register your
